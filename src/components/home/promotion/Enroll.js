9c34065ec19fefcdc8ae0dab49ba1154725e24f9
Hari Basnet <harikrishna.basnet@yahoo.com>
2019-07-10 18:46:34 +0300

    form validation

--- src/components/home/promotion/Enroll.js
+++ src/components/home/promotion/Enroll.js
@@ -41,7 +41,7 @@ class Enroll extends Component {
 
         // console.log(validData)
         newFormdata[element.id] = newElement;
-        console.log(newFormdata)
+
 
         this.setState({
             formdata: newFormdata
@@ -49,7 +49,53 @@ class Enroll extends Component {
 
     }
 
-    submitForm() {
+    resetFormSuccess() {
+        const newFormdata = { ...this.state.formdata }
+
+        for (let key in newFormdata) {
+            newFormdata[key].value = '';
+            newFormdata[key].valid = false;
+            newFormdata[key].validationMessage = '';
+        }
+
+        this.setState({
+            formError: false,
+            formdata: newFormdata,
+            formSuccess: 'congratulations'
+        })
+
+        this.successMessage();
+    }
+
+    successMessage() {
+        setTimeout(() => {
+            this.setState({
+                formSuccess: ''
+            })
+        }, 2000)
+    }
+    submitForm(event) {
+        event.preventDefault();
+
+        let dataToSubmit = {};
+        let formIsValid = true;
+
+        for (let key in this.state.formdata) {
+            dataToSubmit[key] = this.state.formdata[key].value;
+            formIsValid = this.state.formdata[key].valid && formIsValid
+        }
+
+        if (formIsValid) {
+            console.log(dataToSubmit)
+            this.resetFormSuccess();
+
+        } else {
+            this.setState({
+                formError: false,
+                formSuccess: true
+            })
+
+        }
 
     }
     render() {
@@ -66,6 +112,14 @@ class Enroll extends Component {
                                 formdata={this.state.formdata.email}
                                 change={(element) => this.updateForm(element)}
                             />
+                            {this.state.formError ?
+                                <div className="error_label">
+                                    Something is wrong, try again
+                                </div>
+                                : null
+                            }
+                            <div className="success_label">{this.state.formSuccess}</div>
+                            <button onClick={(event) => this.submitForm(event)}>Enroll</button>
                         </div>
 
                     </form>
--- src/components/ui/formFields.js
+++ src/components/ui/formFields.js
@@ -2,6 +2,17 @@ import React from 'react'
 
 const FormField = ({ formdata, id, change }) => {
 
+    const showError = () => {
+        let errorMessage = <div className="error_label">
+            {
+                formdata.validation && !formdata.valid ?
+                    formdata.validationMessage
+                    : null
+            }
+        </div>
+        return errorMessage
+    }
+
     const renderTemplate = () => {
         let formTemplate = null;
         switch (formdata.element) {
@@ -13,6 +24,7 @@ const FormField = ({ formdata, id, change }) => {
                             value={formdata.value}
                             onChange={(event) => change({ event, id })}
                         />
+                        {showError()}
                     </div>
                 )
 
